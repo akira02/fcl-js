52483cd8633c154754cb7ccc44161b68c345d0ad
Greg Santos <greg.santos@dapperlabs.com>
2022-07-19 15:39:30 -0700

    PKG -- [fcl] Update wc util to handle saved pairing and session

--- packages/fcl-wc/src/fcl-wc.js
+++ packages/fcl-wc/src/fcl-wc.js
@@ -1,25 +1,26 @@
-import Client from "@walletconnect/sign-client"
-export {default as QRCodeModal} from "@walletconnect/qrcode-modal"
+import SignClient from "@walletconnect/sign-client"
+import QRCodeModal from "@walletconnect/qrcode-modal"
 export {getSdkError} from "@walletconnect/utils"
 
-const DEFAULT_PROJECT_ID = ""
+const DEFAULT_PROJECT_ID = "6427e017c4bd829eef203702a51688b0"
 const DEFAULT_RELAY_URL = "wss://relay.walletconnect.com"
 const DEFAULT_LOGGER = "debug"
 const DEFAULT_APP_METADATA = {
-  name: "Flow WalletConnect",
-  description: "Flow DApp for WalletConnect",
+  name: "FCL WalletConnect",
+  description: "FCL DApp for WalletConnect",
   url: "https://flow.com/",
   icons: ["https://avatars.githubusercontent.com/u/62387156?s=280&v=4"],
 }
 
-export let client = null
-
-export const initClient = async (projectID, metadata) => {
-  if (client) {
-    return client
+export const wcAdapter = async (projectID, metadata) => {
+  return {
+    client: await initClient(projectID, metadata),
+    QRCodeModal,
   }
+}
 
-  return Client.init({
+const initClient = async (projectID, metadata) => {
+  return SignClient.init({
     logger: DEFAULT_LOGGER,
     relayUrl: DEFAULT_RELAY_URL,
     projectId: projectID || DEFAULT_PROJECT_ID,
--- packages/fcl/src/current-user/exec-service/strategies/utils/wc.js
+++ packages/fcl/src/current-user/exec-service/strategies/utils/wc.js
@@ -1,19 +1,6 @@
 import {config} from "@onflow/config"
-import {QRCodeModal} from "../../../../../../fcl-wc/src/fcl-wc"
 
 const noop = () => {}
-
-const checkPersistedState = async client => {
-  let pairings, storedSession
-  pairings = client.pairing.getAll({active: true})
-
-  // populates (the last) existing session to state
-  if (client.session.length) {
-    const lastKeyIndex = client.session.keys.length - 1
-    storedSession = client.session.get(client.session.keys[lastKeyIndex])
-  }
-  return {pairings, storedSession}
-}
 
 const connectWc = async ({client, QRCodeModal, pairing}) => {
   try {
@@ -24,16 +11,12 @@ const connectWc = async ({client, QRCodeModal, pairing}) => {
         events: ["chainChanged", "accountsChanged"],
       },
     }
-    console.log("connect pairing topic is:", pairing?.topic, pairing)
-    console.log("requiredNamespaces config for connect:", requiredNamespaces)
 
     const {uri, approval} = await client.connect({
       pairingTopic: pairing?.topic,
       requiredNamespaces,
     })
 
-    console.log("connect uri:", uri, "approval:", approval)
-    // Open QRCode modal if a URI was returned (i.e. we're not connecting an existing pairing).
     if (uri) {
       QRCodeModal.open(uri, () => {
         console.log("EVENT", "QR Code Modal closed")
@@ -41,10 +24,6 @@ const connectWc = async ({client, QRCodeModal, pairing}) => {
     }
 
     const session = await approval()
-    console.log("Established session:", session)
-    // await onSessionConnected(session)
-    // Update known pairings after session is connected.
-    // setPairings(client.pairing.getAll({active: true}))
     return session
   } catch (e) {
     console.error("Erroring connecting session", e)
@@ -58,14 +37,15 @@ export async function wc(service, body, opts = {}) {
   const onReady = opts.onReady || noop
   const onResponse = opts.onResponse || noop
   const onClose = opts.onClose || noop
-  const client = await config.get("wc.client")
+  const {client, QRCodeModal} = await config.get("wc.adapter")
   if (typeof client === "undefined") {
     throw new Error("WalletConnect is not initialized")
   }
-  const {pairings, storedSession} = await checkPersistedState(client)
-  console.log("stored pairings", pairings, "stored session:", storedSession)
-
-  let session = storedSession
+  let session
+  if (client.session.length) {
+    const lastKeyIndex = client.session.keys.length - 1
+    session = client.session.get(client.session.keys[lastKeyIndex])
+  }
   if (session == null) {
     let pairing = {topic: service.provider.address ?? undefined}
     session = await connectWc({
@@ -73,12 +53,10 @@ export async function wc(service, body, opts = {}) {
       QRCodeModal,
       pairing,
     })
-    console.log("session connected:", session)
   }
 
   if (service.endpoint === "flow_authn") {
     try {
-      console.log("<--- handle Authn 11 -->", service.endpoint)
       const result = await client.request({
         topic: session.topic,
         chainId: "flow:testnet",
@@ -87,7 +65,6 @@ export async function wc(service, body, opts = {}) {
           params: [],
         },
       })
-      console.log(" handle Authn result ->", result)
       onResponse(result, {
         close: () => QRCodeModal.close(),
       })
@@ -98,13 +75,12 @@ export async function wc(service, body, opts = {}) {
 
   if (service.endpoint === "flow_authz") {
     try {
-      console.log("<--- handle Authz -->", service, body)
       const result = await client.request({
         topic: session.topic,
         chainId: "flow:testnet",
         request: {
           method: service.endpoint,
-          params: JSON.stringify(body),
+          params: JSON.parse(JSON.stringify(body || {})),
         },
       })
 
